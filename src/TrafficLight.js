2b223f0225b3e7a589b25bdd3447f8a45c86a802
‘AMAYA <aamaya547@gmail.com>
2022-06-09 14:26:48 -0700

    removed unused variables

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function TrafficLight(props) {
+export default function TrafficLight() {
   return (
     // this component takes in a prop called color. It is either 'red', 'yellow' or 'green'.
-    <div className='traffic-light'>
+    <div className="traffic-light">
       {/* in each of these divs, set it up so that if the color matches, add the css class 'on' to the classNames */}
       {/* for example, the css classes for the div below should be `red circle on`, if props.color is 'red' */}
       <div className={`red circle`}></div>
